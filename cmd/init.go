eadca1d5f4a8eb565ccca8683c33888f81cbec1b
edentech88 <eddy.reynoso0618@gmail.com>
2021-05-21 11:39:28 +0300

    setup iniit and run init funcs created

---
+++ cmd/init.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"context"
+	"io/ioutil"
+
+	"github.com/edentech88/pastel-utility/configs"
+	"github.com/pastelnetwork/gonode/common/cli"
+	"github.com/pastelnetwork/gonode/common/log"
+	"github.com/pastelnetwork/gonode/common/log/hooks"
+	"github.com/pastelnetwork/gonode/common/sys"
+	"github.com/pkg/errors"
+)
+
+func setupInitCommand(app *cli.App, config *configs.Config) {
+
+	// define flags here
+	var initFlag string
+
+	initCommand := cli.NewCommand()
+	initCommand.Name = "init"
+	app.SetUsage("usage")
+	initCommandFlags := []*cli.Flag{
+		cli.NewFlag("flag-name", &initFlag),
+	}
+	initCommand.AddFlags(initCommandFlags...)
+	addLogFlags(initCommand, config)
+
+	initCommand.SetActionFunc(func(ctx context.Context, args []string) error {
+		ctx = log.ContextWithPrefix(ctx, "app")
+
+		if config.Quiet {
+			log.SetOutput(ioutil.Discard)
+		} else {
+			log.SetOutput(app.Writer)
+		}
+
+		if config.LogFile != "" {
+			fileHook := hooks.NewFileHook(config.LogFile)
+			log.AddHook(fileHook)
+		}
+
+		if err := log.SetLevelName(config.LogLevel); err != nil {
+			return errors.Errorf("--log-level %q, %v", config.LogLevel, err)
+		}
+
+		log.Info("flag-name: ", initFlag)
+
+		return runInit(ctx, config)
+	})
+	app.AddCommands(initCommand)
+}
+
+func runInit(ctx context.Context, config *configs.Config) error {
+	log.WithContext(ctx).Info("Init")
+	defer log.WithContext(ctx).Info("End")
+
+	log.WithContext(ctx).Infof("Config: %s", config)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	sys.RegisterInterruptHandler(cancel, func() {
+		log.WithContext(ctx).Info("Interrupt signal received. Gracefully shutting down...")
+	})
+
+	// actions to run goes here
+
+	return nil
+
+}

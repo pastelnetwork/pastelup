0206e7b9bc015a1c06d4cf55e40e020fc22e17c1
edentech88 <eddy.reynoso0618@gmail.com>
2021-05-21 10:43:26 +0300

    logging setup

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/pastelnetwork/gonode/common/errors"
+	"github.com/pastelnetwork/gonode/common/log"
+	"github.com/pastelnetwork/gonode/common/sys"
+)
 
+const (
+	debugModeEnvName = "PASTEL_UTILITY_DEBUG"
+)
+
+var (
+	debugMode = sys.GetBoolEnv(debugModeEnvName, false)
+)
+
+func main() {
+	defer errors.Recover(log.FatalAndExit)
+
+	log.FatalAndExit(err)
+
+}
+
+func init() {
+	log.SetDebugMode(debugMode)
 }

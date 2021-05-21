1e08208e586b99e54a585eb35cbf86c3f693b6a9
edentech88 <eddy.reynoso0618@gmail.com>
2021-05-21 15:21:11 +0300

    replaced panic with error

--- configs/config.go
+++ configs/config.go
@@ -9,14 +9,14 @@ type Config struct {
 	Main `mapstructure:",squash"`
 }
 
-func (config *Config) String() string {
+func (config *Config) String() (string, error) {
 	// The main purpose of using a custom converting is to avoid unveiling credentials.
 	// All credentials fields must be tagged `json:"-"`.
 	data, err := json.Marshal(config)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return string(data)
+	return string(data), nil
 }
 
 // New returns a new Config instance

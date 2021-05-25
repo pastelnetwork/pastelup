fb1a2f4f7e9dc57dbfbca597e616d9f630b4c3f4
edentech88 <eddy.reynoso0618@gmail.com>
2021-05-25 15:53:14 -0400

    created supernode and walletnode default config

--- configs/config.go
+++ configs/config.go
@@ -4,10 +4,26 @@ import (
 	"encoding/json"
 )
 
+var WalletDefaultConfig = `node:
+	api:
+		hostname: "localhost"
+		port: 8080
+`
+
+var SupernodeDefaultConfig = `node:
+	# ` + `pastel_id` + ` must match to active ` + `PastelID` + ` from masternode.
+	# To check it out first get the active outpoint from ` + `masteronde status` + `, then filter the result of ` + `tickets list id mine` + ` by this outpoint.
+	pastel_id: some-value
+	server:
+		# ` + `listen_address` + ` and ` + `port` + ` must match to ` + `extAddress` + ` from masternode.conf
+		listen_addresses: "127.0.0.1"
+		port: 4444
+`
+
 // Config contains configuration of all components of the WalletNode.
 type Config struct {
-	Main `json:",squash"`
-	Init `json:",squash"`
+	Main `json:","`
+	Init `json:","`
 }
 
 // String : returns string from Config fields

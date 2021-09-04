5a3b0b2d0e59be50d3ac7480998711364ef1ce7f
nguyen202 <nguyen202v@gmail.com>
2021-09-04 19:14:24 +0700

    fix unit test

--- configurer/configurer_test.go
+++ configurer/configurer_test.go
@@ -12,23 +12,23 @@ import (
 
 func TestGetDownloadUrl(t *testing.T) {
 	testConfigurer, _ := NewConfigurer()
-	downloadUrl, fileName, _ := testConfigurer.GetDownloadURL("beta", constants.WalletNode)
+	downloadURL, fileName, _ := testConfigurer.GetDownloadURL("beta", constants.WalletNode)
 	assert.Equal(t, constants.WalletNodeExecName[utils.GetOS()], fileName)
-	assert.Equal(t, fmt.Sprintf("/beta/gonode/%s", constants.WalletNodeExecName[utils.GetOS()]), downloadUrl.Path)
+	assert.Equal(t, fmt.Sprintf("/beta/gonode/%s", constants.WalletNodeExecName[utils.GetOS()]), downloadURL.Path)
 }
 
 func TestGetDownloadGitURL(t *testing.T) {
 	testConfigurer, _ := NewConfigurer()
-	downloadUrl, fileName, _ := testConfigurer.GetDownloadGitURL("v1.0", constants.WalletNode)
+	downloadURL, fileName, _ := testConfigurer.GetDownloadGitURL("v1.0", constants.WalletNode)
 	assert.Equal(t, constants.WalletNodeExecName[utils.GetOS()], fileName)
-	assert.Equal(t, fmt.Sprintf("/pastelnetwork/gonode/releases/download/v1.0/%s", constants.WalletNodeExecName[utils.GetOS()]), downloadUrl.Path)
+	assert.Equal(t, fmt.Sprintf("/pastelnetwork/gonode/releases/download/v1.0/%s", constants.WalletNodeExecName[utils.GetOS()]), downloadURL.Path)
 	fmt.Println(fileName)
 }
 
 func TestGetDownloadGitChecksumURL(t *testing.T) {
 	testConfigurer, _ := NewConfigurer()
-	downloadUrl, fileName, _ := testConfigurer.GetDownloadGitChecksumURL("v1.0", constants.WalletNode)
+	downloadURL, fileName, _ := testConfigurer.GetDownloadGitChecksumURL("v1.0", constants.WalletNode)
 	assert.Equal(t, constants.WalletNodeExecChecksumName[utils.GetOS()], fileName)
-	assert.Equal(t, fmt.Sprintf("/pastelnetwork/gonode/releases/download/v1.0/%s", constants.WalletNodeExecChecksumName[utils.GetOS()]), downloadUrl.Path)
+	assert.Equal(t, fmt.Sprintf("/pastelnetwork/gonode/releases/download/v1.0/%s", constants.WalletNodeExecChecksumName[utils.GetOS()]), downloadURL.Path)
 	fmt.Println(fileName)
 }

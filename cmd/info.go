42089f8041faf51c78bc05cfd1c68a0fb536e640
a-ok123 <ak@keyideagroup.com>
2022-03-26 00:03:22 -0400

    Info command

--- cmd/info.go
+++ cmd/info.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"math"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/cloudfoundry/gosigar"
 	"github.com/olekukonko/tablewriter"
@@ -20,6 +24,7 @@ import (
 var (
 	flagHostInfo   bool
 	flagPastelInfo bool
+	flagOutput     string
 )
 
 var (
@@ -32,6 +37,39 @@ var (
 		constants.DDImgService,
 	}
 )
+
+type processInfo struct {
+	Process   string
+	Pid       string
+	Cpu       string
+	Virtmem   string
+	Rmem      string
+	Starttime string
+	Runtime   string
+	Path      string
+}
+
+type memoryInfo struct {
+	Memory string
+	Total  string
+	Used   string
+	Free   string
+}
+
+type filesystemInfo struct {
+	Filesystem string
+	Size       string
+	Used       string
+	Avail      string
+	Use        string
+	Mounted    string
+}
+
+type pastelInfo struct {
+	MemInfo  []memoryInfo
+	FsInfo   []filesystemInfo
+	ProcInfo []processInfo
+}
 
 type infoCommand uint8
 
@@ -51,10 +89,6 @@ var (
 	}
 )
 
-type processInfo struct {
-	Path string
-}
-
 func setupInfoSubCommand(config *configs.Config,
 	infoCmd infoCommand, remote bool,
 	f func(context.Context, *configs.Config) error,
@@ -65,6 +99,8 @@ func setupInfoSubCommand(config *configs.Config,
 			SetUsage(green("Get Host info (Host name, OS version, ")).SetValue(true),
 		cli.NewFlag("pastel", &flagPastelInfo).
 			SetUsage(green("Get Pastel info (Working Directory, Executables Directory")).SetValue(true),
+		cli.NewFlag("output", &flagOutput).
+			SetUsage(green("How to present information. Available choices are: 'console' and 'json'")).SetValue("console"),
 	}
 
 	remoteFlags := []*cli.Flag{
@@ -133,21 +169,49 @@ func setupInfoCommand() *cli.Command {
 	return infoCommand
 }
 
+func formatMemory(val uint64) string {
+	return strconv.Itoa(int(val / 1024))
+}
+func formatSize(size uint64) string {
+	return sigar.FormatSize(size * 1024)
+}
+
 func runInfoSubCommand( /*ctx*/ _ context.Context, config *configs.Config) error {
 
-	pastelProcNames := make(map[string]processInfo)
 	if flagHostInfo {
 		log.Infof(green("=== System info ==="))
 		host, _ := os.Hostname()
 		log.Infof("HostName: %s", host)
 		log.Infof("OS: %s", utils.GetOS())
 
+		pastelProcNames := make(map[string]bool)
 		for _, tool := range pastelTools {
-			pastelProcNames[constants.ServiceName[tool][utils.GetOS()]] = processInfo{}
+			name := constants.ServiceName[tool][utils.GetOS()]
+			short := int(math.Min(15, float64(len(name))))
+			shortName := name[:short]
+			pastelProcNames[shortName] = true
 		}
 
-		getMemoryInfo()
-		getPastelProcessesInfo(&pastelProcNames)
+		memInfo := getMemoryInfo()
+		fsInfo := getFSInfo()
+		procInfo := getPastelProcessesInfo(&pastelProcNames)
+
+		if flagOutput == "json" {
+			j := pastelInfo{
+				MemInfo:  memInfo,
+				FsInfo:   fsInfo,
+				ProcInfo: procInfo,
+			}
+			data, _ := json.Marshal(j)
+			//if err != nil {
+			//	fmt.Fprintf(os.Stdout, "Error %v\n", err)
+			//}
+			fmt.Fprintf(os.Stdout, "%s\n", string(data))
+		} else {
+			printMemoryInfo(memInfo)
+			printFSInfo(fsInfo)
+			printProcessInfo(procInfo)
+		}
 	}
 
 	if flagPastelInfo {
@@ -156,26 +220,94 @@ func runInfoSubCommand( /*ctx*/ _ context.Context, config *configs.Config) error
 		//}
 
 		log.Infof("Working Directory: %s", config.WorkingDir)
-		log.Infof("Pastel Exec Directory: %s", pastelProcNames[string(constants.PastelD)].Path)
+		//log.Infof("Pastel Exec Directory: %s", pastelProcNames[string(constants.PastelD)].Path)
 	}
 
 	return nil
 }
 
-func runRemoteInfoSubCommand( /*ctx*/ _ context.Context /*config*/, _ *configs.Config) error {
+func runRemoteInfoSubCommand(ctx context.Context, config *configs.Config) error {
+	infoOptions := ""
+	if flagHostInfo {
+		infoOptions = fmt.Sprint(" --host")
+	}
+	if flagPastelInfo {
+		infoOptions = fmt.Sprintf("%s --pastel", infoOptions)
+	}
+	if len(flagOutput) > 0 {
+		infoOptions = fmt.Sprintf("%s --output %s", infoOptions, flagOutput)
+	}
+
+	infoCmd := fmt.Sprintf("%s info %s", constants.RemotePastelupPath, infoOptions)
+
+	if err := executeRemoteCommand(ctx, config, infoCmd, false); err != nil {
+		log.WithContext(ctx).WithError(err).Error("Failed to get info from remote host")
+	}
+
+	//var info []processInfo
+	//err := json.Unmarshal(data, &info)
+	//if err != nil {
+	//	return
+	//}
+
+	//var info []memInfo
+	//err := json.Unmarshal(data, &info)
+	//if err != nil {
+	//	return
+	//}
+
 	return nil
 }
 
-func format(val uint64) uint64 {
-	return val / 1024
-}
-
-func getPastelProcessesInfo(pastelProcNames *map[string]processInfo) {
+func getPastelProcessesInfo(procNames *map[string]bool) []processInfo {
 	pids := sigar.ProcList{}
 	pids.Get()
 
+	var procInfo []processInfo
+	for _, pid := range pids.List {
+		state := sigar.ProcState{}
+		if err := state.Get(pid); err != nil {
+			continue
+		}
+		found := false
+		if _, found = (*procNames)[state.Name]; !found {
+			continue
+		}
+
+		exe := sigar.ProcExe{}
+		if err := exe.Get(pid); err != nil {
+			continue
+		}
+		mem := sigar.ProcMem{}
+		if err := mem.Get(pid); err != nil {
+			continue
+		}
+		time := sigar.ProcTime{}
+		if err := time.Get(pid); err != nil {
+			continue
+		}
+		cpu := sigar.ProcCpu{}
+		if err := cpu.Get(pid); err != nil {
+			continue
+		}
+
+		dir, file := filepath.Split(exe.Name)
+		procInfo = append(procInfo, processInfo{
+			Pid:       strconv.Itoa(pid),
+			Process:   file,
+			Path:      dir,
+			Virtmem:   strconv.Itoa(int(mem.Size / 1024)),
+			Rmem:      strconv.Itoa(int(mem.Resident / 1024)),
+			Starttime: time.FormatStartTime(),
+			Runtime:   time.FormatTotal(),
+			Cpu:       strconv.Itoa(int(cpu.Percent)),
+		})
+	}
+	return procInfo
+}
+func printProcessInfo(info []processInfo) {
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Service name", "Process ID", "CPU %", "Virtual Memory", "Resident Memory", "Starting Time", "Running Time", "Path"})
+	table.SetHeader([]string{"Process", "Pid", "CPU%", "VirtMem", "RMem", "StartTime", "RunTime", "Path"})
 	table.SetColumnColor(
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
@@ -186,72 +318,117 @@ func getPastelProcessesInfo(pastelProcNames *map[string]processInfo) {
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
 	)
-
-	for _, pid := range pids.List {
-
-		state := sigar.ProcState{}
-		if err := state.Get(pid); err != nil {
-			continue
-		}
-
-		var pinfo processInfo
-		found := false
-		if pinfo, found = (*pastelProcNames)[state.Name]; !found {
-			continue
-		}
-
-		exe := sigar.ProcExe{}
-		if err := exe.Get(pid); err != nil {
-			continue
-		}
-		pinfo.Path = exe.Cwd
-		(*pastelProcNames)[state.Name] = pinfo
-
-		mem := sigar.ProcMem{}
-		if err := mem.Get(pid); err != nil {
-			continue
-		}
-
-		time := sigar.ProcTime{}
-		if err := time.Get(pid); err != nil {
-			continue
-		}
-
-		cpu := sigar.ProcCpu{}
-		if err := cpu.Get(pid); err != nil {
-			continue
-		}
-
+	for _, process := range info {
 		table.Append([]string{
-			state.Name,
-			strconv.Itoa(pid),
-			strconv.Itoa(int(cpu.Percent)),
-			strconv.Itoa(int(mem.Size / 1024)),
-			strconv.Itoa(int(mem.Resident / 1024)),
-			time.FormatStartTime(),
-			time.FormatTotal(),
-			exe.Cwd,
+			process.Process,
+			process.Pid,
+			process.Cpu,
+			process.Virtmem,
+			process.Rmem,
+			process.Starttime,
+			process.Runtime,
+			process.Path,
 		})
 	}
 	table.Render()
 }
 
-func getMemoryInfo() {
+func getMemoryInfo() []memoryInfo {
 	mem := sigar.Mem{}
-	swap := sigar.Swap{}
-
 	mem.Get()
+
+	swap := sigar.Swap{}
 	swap.Get()
 
-	fmt.Fprintf(os.Stdout, "%18s %10s %10s\n",
-		"total", "used", "free")
+	return []memoryInfo{
+		{
+			Memory: "RAM",
+			Total:  formatMemory(mem.Total),
+			Used:   formatMemory(mem.Used),
+			Free:   formatMemory(mem.Free),
+		},
+		{
+			Memory: "-/+ buffers/cache",
+			Total:  "",
+			Used:   formatMemory(mem.ActualUsed),
+			Free:   formatMemory(mem.ActualFree),
+		},
+		{
+			Memory: "Swap",
+			Total:  formatMemory(swap.Total),
+			Used:   formatMemory(swap.Used),
+			Free:   formatMemory(swap.Free),
+		},
+	}
+}
+func printMemoryInfo(info []memoryInfo) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Memory", "Total", "Used", "Free"})
+	table.SetColumnColor(
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiGreenColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+	)
+	for _, memInfo := range info {
+		table.Append([]string{
+			memInfo.Memory,
+			memInfo.Total,
+			memInfo.Used,
+			memInfo.Free,
+		})
+	}
+	table.Render()
+}
 
-	fmt.Fprintf(os.Stdout, "Mem:    %10d %10d %10d\n",
-		format(mem.Total), format(mem.Used), format(mem.Free))
+func getFSInfo() []filesystemInfo {
+	fsList := sigar.FileSystemList{}
+	fsList.Get()
 
-	fmt.Fprintf(os.Stdout, "-/+ buffers/cache: %10d %10d\n",
-		format(mem.ActualUsed), format(mem.ActualFree))
+	var fsInfo []filesystemInfo
+	for _, fs := range fsList.List {
 
-	fmt.Fprintf(os.Stdout, "Swap:   %10d %10d %10d\n",
-		format(swap.Total), format(swap.Used), format(swap.Free))
+		if strings.HasPrefix(fs.DevName, "/dev/loop") ||
+			!strings.HasPrefix(fs.DevName, "/dev/") {
+			continue
+		}
+
+		dir_name := fs.DirName
+
+		usage := sigar.FileSystemUsage{}
+		usage.Get(dir_name)
+
+		fsInfo = append(fsInfo, filesystemInfo{
+			Filesystem: fs.DevName,
+			Size:       formatSize(usage.Total),
+			Used:       formatSize(usage.Used),
+			Avail:      formatSize(usage.Avail),
+			Use:        sigar.FormatPercent(usage.UsePercent()),
+			Mounted:    dir_name,
+		})
+	}
+	return fsInfo
+}
+func printFSInfo(info []filesystemInfo) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Filesystem", "Size", "Used", "Avail", "Use%", "Mounted on"})
+	table.SetColumnColor(
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiGreenColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor},
+	)
+	for _, fs := range info {
+		table.Append([]string{
+			fs.Filesystem,
+			fs.Size,
+			fs.Used,
+			fs.Avail,
+			fs.Use,
+			fs.Mounted,
+		})
+	}
+	table.Render()
 }
